308a5127ec3e5d74be0f17125015bcd8c39e614c
hector ladero <hectorladero@gmail.com>
2021-04-20 22:07:12 -0400

    upload code

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoicmxodW5pdmVyc2l0eSIsImEiOiJja25renplNW0wYmswMm9xdnN2NHJpOXNxIn0.DREee-rZtfaBU6UrHTPx6w";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,15 +1,12 @@
-url= 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson'
 
-var map=L.
 
 function createMap(earthquake){
-
-
+    var map= L.map("map-id",{
+        center: [point_coord.coordinates[0], point_coord.coordinates[1]],
+        zoom:12,
+    });
 
 }
-
-
-
 
 function createMarkers(response){
     var point_coords = response.type.coordinates;
@@ -19,5 +16,13 @@ function createMarkers(response){
 
     for (var index=0; index<point_coord; index++){
         var point_coord =point_coords[index];
+    
+    var location =L.marker([point_coord.coordinates[0],point_coords.coordinates[1]]).bindPopup("<h3>" + magnitud + "<h3>");
+    
+    earthquake_data.push(location);
     }
+
+    createMap(L.layerGroup(earthquake_data));
 }
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson").then(createMarkers);
