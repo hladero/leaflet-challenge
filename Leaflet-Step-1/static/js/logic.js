51a0a30537d5c51b86548b5fd333be281f2389e0
hector ladero <hectorladero@gmail.com>
2021-04-25 21:39:30 -0400

    upload code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,46 +1,81 @@
+var map= L.map("map",{
+    center: [40,-10],
+    zoom:3,
+});
 
-    var map= L.map("map",{
-        center: [40,-10],
-        zoom:3,
-    });
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(map);
 
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11",
-        accessToken: API_KEY
-    }).addTo(map);
-
-    function getColor(mag) {
-        switch (true) {
-            case mag>90:
-                return 'red';
-            case mag>60:
-                return 'orange';
-            case mag<61:
-                return 'green';
-        }
+function getColor(mag) {
+    switch (true) {
+        case mag>90:
+            return 'red';
+        case mag>60:
+            return 'orange';
+        case mag<61:
+            return 'green';
     }
+}
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson").then(data=>{
+    console.log(data);
+
+    L.geoJson(data,{pointToLayer:function(quake, latlng) {
+        console.log(quake);
+ 
+        return L.circleMarker(latlng,
+            {
+                radius:quake.properties.mag*4,
+                fillColor:getColor(latlng.alt),
+                color:'black',
+                fillOpacity:1,
+                stroke: .5
+                
+
+
+            });
+        }}).addTo(map);
+
+        L.marker(latlng)
+            .bindPopup("<h1>" + quake.properties.mag + "</h1>")
+            .addTo(map);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // var info=L.control({
+        //     position:"bottomright"
+        // });
+
+        // info.onAdd = function(){
+        //     var div =L.DomUtil.create("div", "legend");
+        //     return div;
+        // };
+        // info.addTo(map);
+        
     
-    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson").then(data=>{
-        console.log(data);
 
 
-
-        L.geoJson(data,{pointToLayer:function(quake, latlng) {
-            console.log(quake);
-            return L.circleMarker(latlng,
-                {
-                    radius:quake.properties.mag*4,
-                    fillColor:getColor(latlng.alt),
-                    color:'black',
-                    fillOpacity:1,
-                    stroke: .5
-                });
-        }}).addTo(map)
-    });
+    
 // function createMap(earthquake){
 
 // }
