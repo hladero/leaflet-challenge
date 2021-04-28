1a92dbd806ab48f236e17f8dedff865d0f471cb4
hector ladero <hectorladero@gmail.com>
2021-04-27 21:52:11 -0400

    upload code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
-var map= L.map("map",{
-    center: [40,-10],
-    zoom:3,
+var map = L.map("map", {
+    center: [40, -10],
+    zoom: 3,
 });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -14,105 +14,46 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 function getColor(mag) {
     switch (true) {
-        case mag>90:
+        case mag > 90:
             return 'red';
-        case mag>60:
+        case mag > 60:
             return 'orange';
-        case mag<61:
+        case mag < 61:
             return 'green';
     }
 }
 
 
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson").then(data=>{
-    console.log(data);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson").then(data => {
 
-    
-    L.geoJson(data,{pointToLayer:function(quake, latlng) {
-        console.log(quake);
-    
 
-        return L.circleMarker(latlng,
-            {
-                radius:quake.properties.mag*4,
-                fillColor:getColor(latlng.alt),
-                color:'black',
-                fillOpacity:1,
-                stroke: .5
-                
-            });
-        }}).addTo(map);
+    L.geoJson(data, {
+        pointToLayer: function (quake, latlng) {
+
+            return L.circleMarker(latlng,
+                {
+                    radius: quake.properties.mag * 4,
+                    fillColor: getColor(latlng.alt),
+                    color: 'black',
+                    fillOpacity: 1,
+                    weight: 1.5
+
+                }).bindPopup(`<h3>${quake.properties.place}</h3><h3>Magnitude: ${quake.properties.mag}</h3>`);
+        }
+    }).addTo(map);
 });
 
 
+var legend = L.control({ position: 'bottomright' });
 
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend');
 
+    div.innerHTML = "<div style='border: 2px solid black;padding: 2px;background-color:red;color:white'> > 90</div>\
+    <div style='border: 2px solid black;padding: 2px;background-color:orange;color:white'> > 60</div>\
+    <div style='border: 2px solid black;padding: 2px;background-color:green;color:white'> < 61</div>"
 
+    return div;
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // var info=L.control({
-        //     position:"bottomright"
-        // });
-
-        // info.onAdd = function(){
-        //     var div =L.DomUtil.create("div", "legend");
-        //     return div;
-        // };
-        // info.addTo(map);
-        
-    
-
-
-    
-// function createMap(earthquake){
-
-// }
-
-// function createMarkers(response){
-//     var point_coords = response.features.coordinates;
-//     var magnitud = response.features.magnitud;
-
-//     var earthquake_data=[];
-
-//     for (var index=0; index<point_coord; index++){
-//         var point_coord =point_coords[index];
-    
-//     var location =L.marker([point_coord.coordinates[0],point_coords.coordinates[1]]).bindPopup("<h3>" + magnitud + "<h3>");
-    
-//     earthquake_data.push(location);
-//     }
-
-//     createMap(L.layerGroup(earthquake_data));
-// }
-
-
-
-
-
-
-
-// var legend = L.control({position: 'bottomright'});
-    // legend.onAdd = function (map) {
-    //     var div = L.DomUtil.create('div', 'info legend');
-    //  } 
-    
-
-    // for (var i = 0; i < grades.length; i++) {
-    //     div.innerHTML += labels.push('<i style="background:'+ getColor(mag[i] + 1) + '></i>' + mag[i] + (mag[i + 1]) ? '&ndash;' + mag[i + 1])
-    // };
-    //      + '<br>' : '+'));
-    //     }
-    //     return div;
-    //     };
+legend.addTo(map);
